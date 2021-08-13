39ae5b40bd896cb8566005e9a400a4c1f98ad6a9
s7rak <hamzihelow3@gmail.com>
2021-08-13 21:00:17 +0300

    add null safety

--- lib/windows_printing.dart
+++ lib/windows_printing.dart
@@ -7,19 +7,29 @@ class WindowsPrinting {
 
   /// get printers name list
   /// Result: [["Printer 1","Printer 2"]]
-  static Future<List<String>> get printersList async {
+  static Future<List<String>?> get printersList async {
     final String list = await _channel.invokeMethod('getPrintersList');
-    return list.split(';');
+
+    // value.insert(0, "غير محدد"),
+
+
+    List<String> lis =   list.split(';');
+
+
+    lis.insert(0, "غير محدد");
+
+    return lis;
   }
 
   /// print PDF file
   /// Result: "1" success else failed
   static Future<String> printPdf(String filePath, String printerName,
-      {int pageNumber, bool landscape}) async {
+      {int? pageNumber, bool landscape = false}) async {
     final String res =
-        await _channel.invokeMethod('printPdf', <String, dynamic>{
+    await _channel.invokeMethod('printPdf', <String, dynamic>{
       'path': filePath,
       'printer': printerName,
+      // ignore: unnecessary_null_comparison
       'number': pageNumber != null ? (pageNumber - 1).toString() : "ALL_PAGES",
       'orientation': landscape ? "LANDSCAPE_PAGE" : "PORTRAIT",
     });
